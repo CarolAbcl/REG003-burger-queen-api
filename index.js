948b21ad127fa23191770931de9c61e4d994fad0
CarolAbcl <carolallende@gmail.com>
2021-08-09 12:30:33 -0400

    conection moongoose

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-/* eslint-disable linebreak-style */
 const express = require('express');
+const mongoose = require('mongoose');
 const config = require('./config');
-const authMiddleware = require('./middleware/auth');
-const errorHandler = require('./middleware/error');
+// const authMiddleware = require('./middleware/auth');
+// const errorHandler = require('./middleware/error');
 const routes = require('./routes');
 const pkg = require('./package.json');
 
-const { port, secret } = config;
+const { port, dbUrl, secret } = config;
 
 const app = express();
-
 app.listen(3000);
+
+// conexion a base de datos
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  // eslint-disable-next-line no-console
+  .then(console.log('Base de datos conectada'))
+  .catch(console.error);
 
 app.set('config', config);
 app.set('pkg', pkg);
 
-// parse application/x-www-form-urlencoded
+// // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(authMiddleware(secret));
-
+// app.use(authMiddleware(secret));
 
 // Registrar rutas
 routes(app, (err) => {
